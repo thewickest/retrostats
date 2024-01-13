49d802b0ca258c1d22113cbed91f7be17e9873a0
thewickest <graubuntu@gmail.com>
2024-01-13 13:04:23 +0100

    Data validation. Date type transformation

--- prisma/migrations/20240110215804_init/migration.sql
+++ prisma/migrations/20240110215804_init/migration.sql
@@ -1,12 +1,14 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `last_name` VARCHAR(191) NULL,
+    `id_nfc` VARCHAR(191) NOT NULL,
 
-  - Added the required column `id_nfc` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `id_nfc` VARCHAR(191) NOT NULL,
-    ADD COLUMN `last_name` VARCHAR(191) NULL;
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Game` (
@@ -22,12 +24,14 @@ CREATE TABLE `Game` (
 
 -- CreateTable
 CREATE TABLE `Session` (
-    `id` INTEGER NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `gameId` INTEGER NOT NULL,
     `userId` INTEGER NOT NULL,
     `initDate` DATETIME(3) NOT NULL,
+    `duration` INTEGER NOT NULL,
+    `score` INTEGER NOT NULL,
 
-    PRIMARY KEY (`gameId`, `userId`)
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
--- src/sessions/dto/create-session.dto.ts
+++ src/sessions/dto/create-session.dto.ts
@@ -1,7 +1,16 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNumber } from 'class-validator';
+
 export class CreateSessionDto {
+  @IsNumber()
   gameId: number;
+  @IsNumber()
   userId: number;
+  @Type(() => Date)
+  @IsDate()
   initDate: string;
+  @IsNumber()
   duration: number;
+  @IsNumber()
   score: number;
 }
