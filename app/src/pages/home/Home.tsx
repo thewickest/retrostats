2cc8b00324b27bc61762e8e1cc99a33f0ed497a6
thewickest <graubuntu@gmail.com>
2024-09-04 18:23:22 +0200

    feat: adding home page

--- app/src/pages/home/Home.tsx
+++ app/src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SideNav from "../../components/sideNav/SideNav";
 import LeaderBoard from "../../components/leaderBoard/LeaderBoard";
 import { useLocation } from "react-router-dom";
@@ -7,10 +7,20 @@ function Home() {
   const location = useLocation();
   const { pathname: route } = location;
   // const [ route, useRoute ] = useState('/sessions');
-  return (
-    <div className='flex w-full bg-bg text-text border-border'>
-      <SideNav />
-      <LeaderBoard route={route}/>
+  const [ page, setPage ]: any = useState(null)
+  useEffect(()=>{
+    fetch('http://localhost:1337/api/home')
+      .then(res => res.json())
+      .then(data => setPage(data.data))
+  }, [])
+  return ( page &&
+    <div>
+      {/* <SideNav /> */}
+      {/* <LeaderBoard route={route}/> */}
+      <h1>{page.attributes?.title}</h1>
+      <body>
+        {page.attributes?.body}
+      </body>
     </div>
     )
 }
