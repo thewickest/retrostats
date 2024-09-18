88880867b2bd36729a8f79088deb1d523449bc62
thewickest <graubuntu@gmail.com>
2024-09-18 16:56:27 +0200

    feature: routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header/>}>
-          <Route path="home" element={<Home />} />
+          <Route path="" element={<Home />} />
           <Route path="leaderboard" element={<LeaderBoard/>} />
         </Route>
       </Routes>
--- app/src/components/header/Header.tsx
+++ app/src/components/header/Header.tsx
@@ -5,8 +5,10 @@ export default function Header () {
     <>
       <nav className="left-0 top-0 z-20 mx-auto flex h-[76px] w-full border-b-4 border-border dark:border-darkNavBorder bg-white dark:bg-darkNavBg p-4 m500:h-16 ">
         <div className="w-1/6"></div>
-        <Link className="text-4xl m900:w-[unset] font-heading m500:text-xl" to="/home">RETROSTATS</Link>
-        <Link to="/leaderboard">Leaderboard</Link>
+        <Link className="text-4xl m900:w-[unset] font-heading m500:text-xl" to="/">RETROSTATS</Link>
+        <div className="flex items-center gap-10 m900:hidden">
+          <Link className="text-xl font-base" to="/leaderboard">Leaderboard</Link>
+        </div>
       </nav>
       <Outlet/>
     </>
