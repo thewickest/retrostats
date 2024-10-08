e69ffc3a7359fd06a762b00e0a36c6da91ec3a1a
thewickest <graubuntu@gmail.es>
2024-10-09 00:02:35 +0200

    Revert "Change player service return type"
    This reverts commit c700723ba63dc62e25a4a440fa3aabee962bb8bf.

--- api/src/players/players.service.ts
+++ api/src/players/players.service.ts
@@ -7,14 +7,13 @@ export class PlayersService {
   constructor(private prismaService: PrismaService) {}
   async findOne(nfc: string) {
     try {
-      const users = await this.prismaService.game_users.findFirst({
+      return this.prismaService.game_users.findFirst({
         where: {
           nfc: {
             equals: nfc,
           },
         },
       });
-      return users ? users : {}
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         throw new HttpException(
