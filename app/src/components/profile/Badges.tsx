46f5b7f26967867241c74f0ab7bec5cb49a0d98e
thewickest <graubuntu@gmail.com>
2024-11-21 17:03:53 +0100

    Small mockup for the profile info

--- app/src/components/profile/Badges.tsx
+++ app/src/components/profile/Badges.tsx
@@ -14,96 +14,13 @@ const Badges = () => {
   const strokeDashoffset = circumference - (percentage / 100) * circumference;
 
   return (
-    <div className="grid grid-cols-3 justify-items-center justify-left">
-      <div className='col-span-1'>
-        <svg
-          width={radius * 2 + strokeWidth}
-          height={radius * 2 + strokeWidth}
-          viewBox={`0 0 ${radius * 2 + strokeWidth} ${radius * 2 + strokeWidth}`}
-        >
-          {/* Background Circle */}
-          {/* <circle
-            cx={radius + strokeWidth / 2}
-            cy={radius + strokeWidth / 2}
-            r={radius}
-            stroke="#e6e6e6"
-            strokeWidth={strokeWidth}
-            fill="none"
-          /> */}
-          {/* Foreground Circle */}
-          <circle
-            cx={radius + strokeWidth / 2}
-            cy={radius + strokeWidth / 2}
-            r={radius}
-            stroke='#9e66ff'
-            strokeWidth={strokeWidth}
-            fill="none"
-            strokeDasharray={circumference}
-            strokeDashoffset={strokeDashoffset}
-            strokeLinecap="butt"
-            style={{ transition: 'stroke-dashoffset 0.5s ease' }}
-          />
-          {/* Text in the middle */}
-          <text
-            x="50%"
-            y="50%"
-            dominantBaseline="middle"
-            textAnchor="middle"
-            fontSize="18"
-            fill="#a388ee"
-          >
-            {level}
-          </text>
-        </svg>
+    <div className="flex justify-items-center justify-left">
+      <div className='flex items-center justify-center w-[45px] h-[45px]'>
+        <img src="/badge.png" alt="" />
       </div>
-      {/* Button 1 */}
-      {/* <button className="flex items-center justify-center w-12 h-12
-      rounded-full border-4 border-gray-500 bg-gray-800 text-white
-      text-3xl font-bold hover:bg-gray-700 transition">
-        +
-      </button> */}
-      <div className='col-span-1'>
-        <svg
-          width={radius * 2 + strokeWidth}
-          height={radius * 2 + strokeWidth}
-          viewBox={`0 0 ${radius * 2 + strokeWidth} ${radius * 2 + strokeWidth}`}
-        >
-          {/* Background Circle */}
-          {/* <circle
-            cx={radius + strokeWidth / 2}
-            cy={radius + strokeWidth / 2}
-            r={radius}
-            stroke="#e6e6e6"
-            strokeWidth={strokeWidth}
-            fill="none"
-          /> */}
-          {/* Foreground Circle */}
-          <circle
-            cx={radius + strokeWidth / 2}
-            cy={radius + strokeWidth / 2}
-            r={radius}
-            stroke='#9e66ff'
-            strokeWidth={strokeWidth}
-            fill="none"
-            strokeDasharray={circumference}
-            strokeDashoffset={strokeDashoffset}
-            strokeLinecap="butt"
-            style={{ transition: 'stroke-dashoffset 0.5s ease' }}
-          />
-          {/* Text in the middle */}
-          <text
-            x="50%"
-            y="50%"
-            dominantBaseline="middle"
-            textAnchor="middle"
-            fontSize="18"
-            fill="#a388ee"
-          >
-            {level}
-          </text>
-        </svg>
+      <div className='flex items-center justify-center w-[45px] h-[45px]'>
+        <img src="/badge.png" alt="" />
       </div>
-      {/* Button 3 */}
       <button className="flex items-center justify-center w-[45px] h-[45px]
       rounded-full text-3xl hover:bg-bg dark:hover:bg-darkBg transition">
         <LuBadgePlus /></button>
--- app/src/components/profile/Level.tsx
+++ app/src/components/profile/Level.tsx
@@ -2,8 +2,8 @@
 import React from 'react';
 
 const Level = ({ level, percentage }: any) => {
-  const radius = 20; // Radius of the circle
-  const strokeWidth = 5; // Width of the circle border
+  const radius = 10; // Radius of the circle
+  const strokeWidth = 3; // Width of the circle border
   const circumference = 2 * Math.PI * radius; // Circumference of the circle
   const color = 'text-green-500'
 
@@ -11,7 +11,7 @@ const Level = ({ level, percentage }: any) => {
   const strokeDashoffset = circumference - (percentage / 100) * circumference;
 
   return (
-    <div className='flex justify-center alig-items-center'>
+    <div className='flex justify-center alig-items-center p-1'>
       <svg
         width={radius * 2 + strokeWidth}
         height={radius * 2 + strokeWidth}
@@ -43,10 +43,12 @@ const Level = ({ level, percentage }: any) => {
         <text
           x="50%"
           y="50%"
+          dy=".05em"
           dominantBaseline="middle"
           textAnchor="middle"
-          fontSize="18"
+          fontSize="12"
           fill="#a388ee"
+          className='p-4'
         >
           {level}
         </text>
