4381f7fc41aa6197425dbb667888065ab05f8d71
thewickest <graubuntu@gmail.com>
2025-04-17 22:13:29 +0200

    feat(api): add swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Retrostats API')
+    .setDescription('An API documentation for the retrostats API')
+    .setVersion('1.0')
+    .addTag('retrostats')
+    .build();
+
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    app.enableCors();
+  
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3001);
 }
 bootstrap();
